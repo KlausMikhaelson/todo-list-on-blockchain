af043bde218753c9939941ab0a26fb54301ad1ef
KlausMikhaelson <satyamsingh5076@gmail.com>
2023-03-27 13:22:08 +0530

    styled the task list

--- components/Tasks.js
+++ components/Tasks.js
@@ -2,11 +2,13 @@ import React from 'react'
 
 const Tasks = ({ taskText, onClick }) => {
     return (
-        <div>
-            <div>
-                {taskText}
+        <div style={{ display: "flex", alignItems: "center", justifyContent: "center", marginTop: "5%" }}>
+            <div style={{ display: "flex" }}>
+                <div style={{ margin: "10px" }}>
+                    {taskText}
+                </div>
+                <button style={{cursor: "pointer"}} onClick={onClick}>Delete</button>
             </div>
-            <button onClick={onClick}>Delete</button>
         </div>
     )
 }
--- pages/Todo.js
+++ pages/Todo.js
@@ -1,17 +1,19 @@
 import Tasks from '@/components/Tasks'
 import React from 'react'
 
-const Todo = ({deleteTask,tasks, input,setInput, addtask}) => {
+const Todo = ({ deleteTask, tasks, input, setInput, addtask }) => {
   console.log(tasks)
   return (
     <div>
-      <input value={input} onChange={e => setInput(e.target.value)} type="text" placeholder="Enter task" />
-      <button onClick={addtask}>Add Task</button>
-    {
-      tasks.map(todo => (
-        <Tasks key={todo.id} taskText={todo.taskText} onClick={deleteTask(todo.id)} />
-      ))
-    }
+      <div style={{display: "flex", alignItems: "center", justifyContent: "center"}}>
+        <input style={{margin: "10px", height: "30px"}} value={input} onChange={e => setInput(e.target.value)} type="text" placeholder="Enter task" />
+        <button onClick={addtask} style={{height: "30px"}}>Add Task</button>
+      </div>
+      {
+        tasks.map(todo => (
+          <Tasks key={todo.id} taskText={todo.taskText} onClick={deleteTask(todo.id)} />
+        ))
+      }
     </div>
   )
 }
